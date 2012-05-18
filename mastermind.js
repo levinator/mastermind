9c715c8e32845238e798733cf3a1d2e0e30d7938
kind <kind@hugo.(none)>
2012-05-18 11:38:03 +0200

    first steps into DOM selection and manipulation with jQuery

---
+++ mastermind.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   $('#hippo').append('<p>leer</p>');
+   $('table#brett').append(' <tr><th></th><td></td><td class="s3s3"></td><td></td><td></td></tr>');
+   $('.s3s3').append('<p>zorro</p>');
+   
+ });
