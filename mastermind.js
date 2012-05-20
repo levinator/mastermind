10a8213b0ea31325bf3d904e4503e377ff4ccfd1
kind <kind@hugo.(none)>
2012-05-20 13:27:56 +0200

    programatically generating the board; (nested) for loops

--- mastermind.js
+++ mastermind.js
@@ -1,6 +1,26 @@
+var felder = 8;
+var zuege = 30;
+var palette = ['blue', 'red', 'green', 'orange', 'purple', 'yellow'];
+var farben = palette.length;
+
+var neues_brett = function(breite, hoehe) {
+  var brett = $('table#brett');
+  var colgroup = $('<colgroup></colgroup>');
+  var geheim = $('<tr id="geheim"><td id="zaehler"></td></tr>');
+  for (var i = 0; i < breite; i++) {
+    colgroup.append($('<col class="brett"/>'));
+    geheim.append($('<th></th>'));
+  }
+  brett.append(colgroup);
+  brett.append(geheim);
+  for (var j = 0; j < hoehe; j++) {
+    var zeile = $('<tr><th></th></tr>');
+    for (var i = 0; i < breite; i++) {
+      zeile.append($('<td></td>'));
+    }
+    brett.append(zeile);
+  }
+}
 $(document).ready(function(){
-   $('#hippo').append('<p>leer</p>');
-   $('table#brett').append(' <tr><th></th><td></td><td class="s3s3"></td><td></td><td></td></tr>');
-   $('.s3s3').append('<p>zorro</p>');
-   
- });
+  neues_brett(felder, zuege);
+});
