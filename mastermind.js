d5a4eca9a44b1f73bbd59c09fe14533f07a39631
Gerrit Imsieke <gerrit.imsieke@gmail.com>
2013-02-18 19:50:20 +0100

    color picking

--- mastermind.js
+++ mastermind.js
@@ -25,14 +25,54 @@ var neues_brett = function(breite, hoehe) {
     farbwahl.append($('<td style="background-color:' + palette[f] + '"></td>'));
   }
   brett.data('aktuell', id(1,1));
+  brett.data('breite', breite);
+  brett.data('hoehe', hoehe);
+  faerbe_aktuell(brett, id(1,1));
 }
 var id = function(x,y) {
   return '_' + x + '_' + y;
 }
 
+var faerbe_aktuell = function(brett, id) {
+  brett.find('#' + id).css('background-color', '#dda');
+  brett.data('aktuell', id);
+}
+
 var einfaerbung = function(brett, farbe) {
-  $('#' + brett.data('aktuell')).css('background-color', farbe);
-  alert('aktuell: ' + brett.data('aktuell') + '. Farbe: ' + farbe);
+  var aktuell = $('#' + brett.data('aktuell'));
+  aktuell.css('background-color', farbe);
+  var naechstes_in_zeile = aktuell.next();
+  if (naechstes_in_zeile.size() == 0) {
+    var auswertung = werte_aus(aktuell.parent(), brett.find('#geheim'));
+    var naechste_zeile = aktuell.parent().prev();
+    if (naechste_zeile.attr('id') == 'geheim') {
+      if (auswertung == false) {
+        game_over();
+      } else {
+        glueckwunsch();
+      }
+    } else {
+      var erste_zelle = naechste_zeile.find('td:first-of-type');
+      faerbe_aktuell(brett, erste_zelle.attr('id'));
+    }
+  } else {
+    faerbe_aktuell(brett, naechstes_in_zeile.attr('id'));
+  }
+}
+
+var werte_aus = function(auszuwertend, geheim) {
+  for (var i = 1; i <= geheim.find('th').size(); i++) {
+    alert(i);
+  }
+  return true;
+}
+
+var glueckwunsch = function() {
+  alert('Herzlichen Glückwunsch!');
+}
+
+var game_over = function() {
+  alert('Schade. Nochmal versuchen?');
 }
 
 
